6f7cf06bd3eabef2c0c0b8b4de9a8e4fdf5d03f8
Patricia <patysiq@yahoo.com.br>
2021-06-20 11:00:54 -0300

    Initial Commit

---
+++ DanceNow/DanceNowApp.swift
@@ -0,0 +1,20 @@
+//
+//  DanceNowApp.swift
+//  DanceNow
+//
+//  Created by PATRICIA S SIQUEIRA on 20/06/21.
+//
+
+import SwiftUI
+
+@main
+struct DanceNowApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
